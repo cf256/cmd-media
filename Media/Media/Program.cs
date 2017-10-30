6817cc713a945b16741ae9ef5df2cb8a156b4f1f
Christian Linchausen <cflinchausen@gmail.com>
2017-10-30 16:58:03 +0100

    Initial Commit

---
+++ Media/Media/Program.cs
@@ -0,0 +1,37 @@
+﻿using WindowsInput;
+namespace Play
+{
+    class Program
+    {
+        static int Main(string[] args)
+        {
+
+            if (args.Length == 0)
+            {
+                return 0;
+            }
+
+            var input = args[0].ToLower();
+            var simulator = new InputSimulator().Keyboard;
+
+            switch (input)
+            {
+                case "play":
+                case "pause":
+                    simulator.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PLAY_PAUSE);
+                    break;
+                case "next":
+                    simulator.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_NEXT_TRACK);
+                    break;
+                case "prev":
+                    simulator.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PREV_TRACK);
+                    simulator.KeyPress(WindowsInput.Native.VirtualKeyCode.MEDIA_PREV_TRACK);
+                    break;
+                default:
+                    break;
+            }
+
+            return 0;
+        }
+    }
+}
